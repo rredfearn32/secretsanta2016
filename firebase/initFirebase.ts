74ce3e53ac5d4d21b124a58a29ab96df56747c43
Robbie Redfearn <rredfearn32@live.com>
2022-09-29 21:16:55 +0200

    Add Firebase helper functions

---
+++ firebase/initFirebase.ts
@@ -0,0 +1,25 @@
+import { FirebaseOptions, getApp, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig: FirebaseOptions = {
+  // ...various env variables
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+function createFirebaseApp(config: FirebaseOptions) {
+  try {
+    return getApp();
+  } catch {
+    return initializeApp(config);
+  }
+}
+
+const firebaseApp = createFirebaseApp(firebaseConfig);
+export const FBauth = getAuth(firebaseApp);
+export const FBfirestore = getFirestore(firebaseApp);
